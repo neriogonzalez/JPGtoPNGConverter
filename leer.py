43f4b958ac8101b23c7a00a94b5014f4726e74db
Nerio <neriogonzalez@gmail.com>
2020-09-07 20:21:30 -0300

    resuelto en un 99%

--- leer.py
+++ leer.py
@@ -1,25 +1,47 @@
+import sys
 import os
-import shutil
+# import shutil
 from PIL import Image
 
-dir1 = '\\Pokedex'
-dir2 = '\\Nuevo'
+# 1.- use sys to grab first and second argumatn
+dir1 = sys.argv[1]
+dir2 = sys.argv[2]
+print('directorio origen:' + dir1 + ', directorio destino:' + dir2)
+
+
+# 2.- # check if exist, if not created new dir
+
+#dir1 = '\\Pokedex'
+#dir2 = '\\Nuevo\\'
+
 var_path = (os.getcwd() + '\images\JPGtoPNGConverter')
+dir1 = var_path + '\\' + dir1
+dir2 = var_path + '\\' + dir2
+print(f'dir1:  {dir1}')
+print(f'dir2:  {dir2}')
+
 os.chdir(var_path)
+
+
 # files = [f for f in os.listdir('.') if os.path.isfile(f)]
 
-if not os.path.exists(var_path+dir1):
-    os.mkdir(var_path+dir1)
 
-if not os.path.exists(var_path+dir2):
-    os.mkdir(var_path+dir2)
+if not os.path.exists(dir1):
+    os.mkdir(dir1)
 
-for f in os.listdir(var_path+dir1):
-    print(var_path+dir1+'\\'+f)
-    print(var_path+dir2+'\\'+f)
-    shutil.copy(var_path+dir1+'\\'+f, var_path+dir2+'\\'+f)
-#img = Image.open(f)
+if not os.path.exists(dir2):
+    os.mkdir(dir2)
 
-# print(f)
-#img.save(var_path+dir2+f+'.png', '.png')
-# print(f)
+
+# 3.- loop into pokedex, conver images, move to new folder
+os.chdir(dir1)
+for f in os.listdir(dir1):
+    print('path: ' + os.getcwd())
+
+    img = Image.open(f)
+    nuevo_nombre = f[0: len(f) - 4:1]
+    print(f'el nuev nombre es: {nuevo_nombre}')
+    print(f'{f} + abierto')
+    print(f'dir2: {dir2}')
+    img.save(dir2+nuevo_nombre+'.png', 'PNG')
+    print(f'{f} + salvado')
