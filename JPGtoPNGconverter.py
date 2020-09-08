b77579147f0cbadd3f6ecc8c5be950b846c6a68b
Nerio <neriogonzalez@gmail.com>
2020-09-07 21:02:55 -0300

    final

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -14,6 +14,5 @@ if not os.path.exists(dir2):
 # 3.- loop into pokedex, convert images, move to new folder
 for file_name in os.listdir(dir1):
     img = Image.open(f'{dir1}{file_name}')
-    nuevo_nombre = os.path.splitext(file_name)
-    img.save(f'{dir2}{nuevo_nombre[0]}.png', 'PNG')
-    print(f'All done')
+    nuevo_nombre = os.path.splitext(file_name)[0]
+    img.save(f'{dir2}{nuevo_nombre}.png', 'PNG')
